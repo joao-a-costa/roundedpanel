32a83ee24127baae30a3be1f14d8b78fd8232b68
Rodrigo Guimarães <rg@smartdigit.pt>
2026-02-25 11:37:13 -0100

    Fix commit

--- RoundedPanel.Test/FormTest.Designer.cs
+++ RoundedPanel.Test/FormTest.Designer.cs
@@ -29,43 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.roundedPanel1 = new RoundedPanel(this.components);
-            this.roundedPanel2 = new RoundedPanel(this.components);
-            this.roundedPanel3 = new RoundedPanel(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.roundedPanel1.SuspendLayout();
+            this.roundedPanel3 = new RoundedPanel.RoundedPanel(this.components);
             this.SuspendLayout();
-            // 
-            // roundedPanel1
-            // 
-            this.roundedPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.roundedPanel1.BorderColor = System.Drawing.Color.Red;
-            this.roundedPanel1.BorderRadius = 25;
-            this.roundedPanel1.BorderThickness = 2;
-            this.roundedPanel1.Controls.Add(this.textBox1);
-            this.roundedPanel1.Location = new System.Drawing.Point(149, 51);
-            this.roundedPanel1.Name = "roundedPanel1";
-            this.roundedPanel1.RadiusBottomLeft = 25;
-            this.roundedPanel1.RadiusBottomRight = 25;
-            this.roundedPanel1.RadiusTopLeft = 25;
-            this.roundedPanel1.RadiusTopRight = 25;
-            this.roundedPanel1.Size = new System.Drawing.Size(299, 47);
-            this.roundedPanel1.TabIndex = 0;
-            // 
-            // roundedPanel2
-            // 
-            this.roundedPanel2.BackColor = System.Drawing.Color.LightBlue;
-            this.roundedPanel2.BorderColor = System.Drawing.Color.DimGray;
-            this.roundedPanel2.BorderRadius = 60;
-            this.roundedPanel2.BorderThickness = 1;
-            this.roundedPanel2.Location = new System.Drawing.Point(573, 148);
-            this.roundedPanel2.Name = "roundedPanel2";
-            this.roundedPanel2.RadiusBottomLeft = 1;
-            this.roundedPanel2.RadiusBottomRight = 1;
-            this.roundedPanel2.RadiusTopLeft = 60;
-            this.roundedPanel2.RadiusTopRight = 60;
-            this.roundedPanel2.Size = new System.Drawing.Size(200, 100);
-            this.roundedPanel2.TabIndex = 3;
             // 
             // roundedPanel3
             // 
@@ -73,7 +38,14 @@
             this.roundedPanel3.BorderColor = System.Drawing.Color.DimGray;
             this.roundedPanel3.BorderRadius = 30;
             this.roundedPanel3.BorderThickness = 1;
-            this.roundedPanel3.Location = new System.Drawing.Point(68, 221);
+            this.roundedPanel3.CollapseOnHeaderClick = true;
+            this.roundedPanel3.HeaderBottomSeparator = true;
+            this.roundedPanel3.HeaderColor = System.Drawing.Color.SteelBlue;
+            this.roundedPanel3.HeaderGradientEndColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.roundedPanel3.HeaderGradientStartColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(86)))), ((int)(((byte)(110)))));
+            this.roundedPanel3.HeaderIcon = null;
+            this.roundedPanel3.IsCollapsed = false;
+            this.roundedPanel3.Location = new System.Drawing.Point(188, 70);
             this.roundedPanel3.Name = "roundedPanel3";
             this.roundedPanel3.RadiusBottomLeft = 30;
             this.roundedPanel3.RadiusBottomRight = 30;
@@ -81,18 +53,8 @@
             this.roundedPanel3.RadiusTopRight = 30;
             this.roundedPanel3.Size = new System.Drawing.Size(279, 204);
             this.roundedPanel3.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(11, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(278, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Texto pretendido...";
+            this.roundedPanel3.TextHeaderColor = System.Drawing.Color.White;
+            this.roundedPanel3.TextHeaderFont = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             // 
             // FormTest
             // 
@@ -100,24 +62,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.roundedPanel2);
             this.Controls.Add(this.roundedPanel3);
-            this.Controls.Add(this.roundedPanel1);
             this.Name = "FormTest";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormTest_Load);
-            this.roundedPanel1.ResumeLayout(false);
-            this.roundedPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private RoundedPanel roundedPanel1;
         private RoundedPanel roundedPanel3;
-        private RoundedPanel roundedPanel2;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
